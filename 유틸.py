ffe7d926fc7ecf7d5b392483ec62d99f1ca54f05
mellbrown <2mellbrown@gmail.com>
2019-04-21 20:42:16 +0900

    2. 영상화질 조절 및 픽셀 처리 기법 완성

--- 유틸.py
+++ 유틸.py
@@ -3,6 +3,7 @@ from scipy import signal, misc
 import matplotlib.pyplot as plt
 from scipy import ndimage
 import cv2
+from mpl_toolkits.mplot3d import Axes3D
 
 # 0. 자료형 처리
 imf32 = lambda im8: np.float32(im8)
@@ -12,7 +13,7 @@ imu8 = lambda im32: np.uint8(np.where(im32 > 255, 255, np.where(im32 < 0, 0, im3
 _implot_i = 0
 _implot_size = (1, 1)
 
-def implot(im8, title=''):
+def implot(im8, title=None):
     global _implot_i
     global _implot_size
     y, x = _implot_size
@@ -21,7 +22,7 @@ def implot(im8, title=''):
     plt.imshow(im8)
     plt.gray()
     plt.axis('off')
-    plt.title(title)
+    plt.title(title if title is not None else str(_implot_i))
 
 def setimplot(h, w, i = 0):
     global _implot_i
@@ -29,20 +30,41 @@ def setimplot(h, w, i = 0):
     _implot_i = i
     _implot_size = (h, w)
 
+def im3D (im8):
+    x = np.arange(0, 256, 1.0)
+    y = np.arange(0, 256, 1.0)
+    X, Y = np.meshgrid(x, y)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_surface(X, Y, im8)
+
 imshow = lambda: plt.show()
 imload8 = lambda name: misc.imread(name)
-imsave8 = lambda name, img: misc.imsave(name, img)
+imsave8 = lambda name, im8: misc.imsave(name, im8)
 
-# 2. 영상화질 조절 및 픽셀 처리 기
+# 2. 영상화질 조절 및 픽셀 처리 기법
 영상반전8 = lambda im8: 255 - im8
 영상밝기조절32 = lambda im32, 밝기: im32 + 밝기
 영상명암비조절32 = lambda im32, 명암비: im32 + (im32 - 128) * 명암비
 영상감마보정32 = lambda im32, 감마: np.float_power(im32 / 255, 감마) * 255
 
-영상중간강조32 = lambda im32, 중간1, 중간2, 강조: \
-    np.where(im32 > 중간2, im32, np.where(im32 < 중간1, im32, im32 + 강조))
-영상이진화8 = lambda im8, 임계값: np.where(im8 > 임계값, 255, 0)
-
+영상중간강조32 = lambda im32, 임계1, 임계2, 강조: \
+    np.where(임계1 <= im32, np.where(im32 < 임계2, im32 + 강조, im32), im32)
+영상이진화8 = lambda im8, 임계: np.where(임계 <= im8, 255, 0)
+영상슬라이스8 = lambda im8, 임계1, 임계2: np.where(임계1 <= im8, np.where(im8 < 임계2, 255, im8), im8)
+def 영상등명도선8 (im8, 임계s, 시작=0):
+    result = np.ones(shape=im8.shape) * 시작
+    for 임계 in 임계s:
+        시작 = 255 - 시작
+        result = np.where(im8 < 임계, result, 시작)
+    return result
+def 영상명도단계변환8 (im8, 임계s):
+    result = np.zeros(shape=im8.shape)
+    for 임계 in 임계s:
+        result = np.where(im8 < 임계, result, 임계)
+    return result
+영상중간제거 = lambda im8, 임계1, 임계2: np.where(임계1 <= im8, im8, np.where(im8 < 임계2, 0, im8))
+영상중간통과 = lambda im8, 임계1, 임계2: np.where(임계1 <= im8, 0, np.where(im8 < 임계2, im8, 0))
 
 # 3. 영상의 산술 및 논리 연산
 영상덧셈32 = lambda im32f, im32g: im32f + im32g
